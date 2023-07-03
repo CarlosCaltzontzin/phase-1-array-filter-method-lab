fc8cdd7c7c3b03edee6e9fbb91ffbb70ec7f1ec4
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-07-03 00:20:02 -0500

    completed phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+function findMatching(drivers, searchString) {
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase().includes(searchString.toLowerCase())
+    );
+  
+    return matchingDrivers;
+  }
+  
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+  const searchTerm = 'bobby';
+  
+  const matchingList = findMatching(drivers, searchTerm);
+  console.log(JSON.stringify(matchingList) === JSON.stringify(['Bobby', 'Bobby'])); // Output: false
+ 
+  function fuzzyMatch(drivers, searchString) {
+    const matchingDrivers = drivers.filter(driver => {
+      const driverLowerCase = driver.toLowerCase();
+      const searchLowerCase = searchString.toLowerCase();
+      return driverLowerCase.indexOf(searchLowerCase) === 0;
+    });
+  
+    return matchingDrivers;
+  }
+  
+  function matchName(drivers, searchString) {
+    const matchingDrivers = drivers.filter(driver => {
+      const driverName = driver.name.toLowerCase();
+      const searchName = searchString.toLowerCase();
+      return driverName === searchName;
+    });
+  
+    return matchingDrivers;
+  }
+  
